d40fa9699c07524352d3bff82280d949d3171567
“Evan <frommere@uchicago.edu>
2022-05-05 09:55:18 -0400

    phase 1

--- class_work/Art Share API/art_share_api/app/models/artwork.rb
+++ class_work/Art Share API/art_share_api/app/models/artwork.rb
@@ -9,4 +9,24 @@ class Artwork < ApplicationRecord
         message: '1 per artist'
     }
 
+    belongs_to(
+        :artist,
+        class_name: 'User',
+        foreign_key: :artist_id,
+        primary_key: :id
+    )
+    has_many(
+        :views,
+        class_name: 'ArtworkShares',
+        foreign_key: :artwork_id,
+        primary_key: :id
+    )
+    has_many(
+        :shared_viewers,
+        through: :views,
+        source: :viewer
+    )
+
+
+
 end
--- class_work/Art Share API/art_share_api/app/models/user.rb
+++ class_work/Art Share API/art_share_api/app/models/user.rb
@@ -3,4 +3,23 @@
 class User <ApplicationRecord
     validates :username , presence: true, uniqueness: true
 
+    has_many(
+        :artworks,
+        class_name: 'Artwork',
+        foreign_key: :artist_id,
+        primary_key: :id
+    )
+    has_many(
+        :views,
+        class_name: 'ArtworkShares',
+        foreign_key: :viewer_id,
+        primary_key: :id
+    )
+    has_many(
+        :shared_artworks,
+        through: :views,
+        source: :artwork
+
+    )
+
 end 
